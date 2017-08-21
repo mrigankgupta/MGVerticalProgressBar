636d65dafcd3e03d04ea58706ea1008104f37b9a
Mrigank Gupta <mrigankgupta@gmail.com>
2017-08-21 22:03:17 +0530

    Creating example for types of progress bar

--- MGVerticalProgressBar/ViewController.swift
+++ MGVerticalProgressBar/ViewController.swift
@@ -17,9 +17,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var percent:Float = 100.0
         timer = Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true, block: { [weak self](timer) in
-            let p = self?.getRandomPercent()
+            var p = self?.getRandomPercent()
             self?.pBar1.progress = p!
+            
+            p = self?.getRandomPercent()
+            self?.pBar2.progress = p!
+            
+            self?.pBar3.progress = percent/100.0
+            print("\(percent)")
+            self?.pBar3.percentText = "\(Int(percent))"
+            percent = percent >= 10 ? percent-10 : 100
         })
     }
 
